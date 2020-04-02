f873d5890cc1f312d08fc77e1dfee53f2e162429
Daniel Rosel <danalves24@outlook.com>
2020-04-02 13:48:44 +0200

    customization of react

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import TraveBot from './travelBotSourceCode.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TraveBot />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('traveBotCode')
 );
 
 // If you want your app to work offline and load faster, you can change
